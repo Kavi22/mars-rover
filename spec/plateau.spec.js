dd414be5097375e53cfadcfb645f225edba31840
Kavita <kavi.b0208@gmail.com@github.com>
2018-02-23 16:04:23 +0000

    adds multiple drones to plateau

--- spec/plateau.spec.js
+++ spec/plateau.spec.js
@@ -9,5 +9,24 @@ describe.only('Rover', () => {
     let plateau1 = new Plateau();
     expect(plateau1).to.be.an.instanceof(Plateau);
   });
+  it('should set plateau size', () => {
+    let plateau1 = new Plateau(5, 5);
+    expect(plateau1.width).to.equal(5);
+    expect(plateau1.height).to.equal(5);
+  });
+  it('should add a rover to the plateau', () => {
+    let plateau1 = new Plateau(5, 5);
+    plateau1.addRover(2,2, 'E');
+    expect(plateau1.rovers.length).to.equal(1);
+    expect(plateau1.rovers).to.be.an('Array');
+    expect(plateau1.rovers[0]).to.be.an('Object');
+  });
+
+  it('should add mulitple rovers to the plateau', () => {
+    let plateau1 = new Plateau(5, 5);
+    plateau1.addRover(2,2, 'E');
+    plateau1.addRover(5,1, 'W');
+    expect(plateau1.rovers.length).to.equal(2);
+  });
 
 });
--- src/Plateau.js
+++ src/Plateau.js
@@ -1,8 +1,15 @@
+const Rover = require('./Rover');
 
 class Plateau  {
   constructor(width, height) {
    this.width = width,
-   this.height = height;
+   this.height = height,
+   this.rovers = [];
+  }
+
+  addRover(xPosition, yPosition, direction) {
+    const newRover = new Rover(xPosition, yPosition, direction);
+    this.rovers.push(newRover);
   }
 
 }
