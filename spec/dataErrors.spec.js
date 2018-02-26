eef1d97fab7c132c4010e89bc24a80bc09213304
Kavita <kavi.b0208@gmail.com@github.com>
2018-02-26 13:31:56 +0000

    will not add rover if co-ordinates are larger than plateau size

--- spec/dataErrors.spec.js
+++ spec/dataErrors.spec.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const DataError = require('../src/DataError');
-const Rover = require('../src/Rover');
-// const Plateau = require('../src/Plateau');
+// const Rover = require('../src/Rover');
+const Plateau = require('../src/Plateau');
 
 describe('DataError', () => {
   it('is a function ', () => {
@@ -13,11 +13,25 @@ describe('DataError', () => {
     expect(e).to.be.an.instanceof(DataError);
   });
 
-  it.skip('creates and saves an error if invalid commmand is passed to rover', () => {
-    let rover1 = new Rover(1, 5, 'S');
-    rover1.saveCommands('S');
-    rover1.moveOrTurn();
-    expect(rover1.errors[0].data).to.equal('S');
+  // it.skip('creates and saves an error if invalid commmand is passed to rover', () => {
+  //   let rover1 = new Rover(1, 5, 'S');
+  //   rover1.saveCommands('S');
+  //   rover1.moveOrTurn();
+  //   expect(rover1.errors[0].data).to.equal('S');
+  // });
+
+  it('will not add rover if co-ordinates are outside of boundry', () => {
+    let p = new Plateau(5,5);
+    p.addRover(6, 5, 'S');
+    expect(p.errors[0].data).to.equal(6);
+    expect(p.rovers.length).to.equal(0);
+  });
+
+  it('will add if rover co-ordinates are inside of boundry', () => {
+    let p = new Plateau(5,5);
+    p.addRover(1, 5, 'S');
+    expect(p.errors.length).to.equal(0);
+    expect(p.rovers.length).to.equal(1);
   });
 
   // it('creates and saves an error if invalid direction is passed to rover', () => {
--- src/Plateau.js
+++ src/Plateau.js
@@ -1,17 +1,32 @@
 const Rover = require('./Rover');
+const DataError = require('./DataError');
 
-class Plateau  {
+class Plateau {
   constructor(width, height) {
-   this.width = width,
-   this.height = height,
-   this.rovers = [];
+    this.width = width,
+      this.height = height,
+      this.rovers = [];
+    this.errors = [];
   }
 
   addRover(xPosition, yPosition, direction) {
-    const newRover = new Rover(xPosition, yPosition, direction);
-    newRover.boundryY = this.height;
-    newRover.boundryX = this.width;
-    this.rovers.push(newRover);
+      if (this.isInBoudary(xPosition, yPosition, direction)) {
+        const newRover = new Rover(xPosition, yPosition, direction);
+        newRover.boundryY = this.height;
+        newRover.boundryX = this.width;
+        this.rovers.push(newRover);
+      }
+  }
+
+  isInBoudary(xPosition, yPosition) {
+    let isInB = true;
+    if (xPosition > this.width || yPosition > this.height) {
+      let e = new DataError('Rover co-ordinates out of grid. Can not add', xPosition, yPosition);
+      this.errors.push(e);
+      console.log(`${e.message}: ${e.data}`);
+      isInB = false;
+    }
+    return isInB;
   }
 
   sendCommands(commands) {
